192b407b6f13fabf581a44c9c890acbee63f82d7
Chandru Palaniyandi <chandra@lucasware.com>
2023-12-25 08:11:21 +0000

    Implemented Publish and SendCommand for the Banking MicroService

--- MicroRabbit/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 
 namespace MicroRabbit.Banking.Application.Interfaces
@@ -10,5 +6,6 @@ namespace MicroRabbit.Banking.Application.Interfaces
     public interface IAccountService
     {
         IEnumerable<Account> GetAccounts();
+        void Transfer(AccountTransfer accountTransfer);
     }
 }
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Events/TransferCreatedEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Banking.Domain.Events
+{
+    public class TransferCreatedEvent : Event
+    {
+        public int From { get; protected set; }   
+        public int To { get; protected set; }
+        public decimal Amount { get; protected set; }
+
+        public TransferCreatedEvent(int from, int to, decimal amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
